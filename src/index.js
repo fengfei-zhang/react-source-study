b7eff7232a0aca31b3b84fd8aaa2266b798edac0
zhangfengfei <zff19970816@163.com>
2022-05-09 15:57:20 +0800

    改react版本为17.0.0，配合卡颂视频学习

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/*
+ * @Author: zhangfengfei
+ * @Date: 2022-05-09 11:54:42
+ * @LastEditTime: 2022-05-09 15:38:26
+ * @LastEditors: zhangfengfei
+ */
+// import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
